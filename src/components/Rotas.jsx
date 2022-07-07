a0e72a2b57fa0dae4c8e03dae4064eb0dd0fb821
Rafael A Goncalves <rafaelandradegs@gmail.com>
2022-07-07 15:05:09 -0300

    Trabalhando com componentes

---
+++ src/components/Rotas.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Rotas = () => {
+  return (
+    <>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/produtos">Produtos</a>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default Rotas
---
+++ src/components/Titulos.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Titulos = ({ titulo }) => {
+  return <h1 style={{ color: 'green' }}>{titulo}</h1>
+}
+
+export default Titulos
