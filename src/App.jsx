386e971a580d0fc86284aafd2b0255bfb97b3c28
Rafael A Goncalves <rafaelandradegs@gmail.com>
2022-07-11 15:27:18 -0300

    Treinando useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import ButtonModal from './ButtonModal'
-import Modal from './Modal'
 
 const App = () => {
-  const [modal, setModal] = React.useState(false)
+  const [contar, setContar] = React.useState(1)
+  const [items, setItems] = React.useState(['item 1'])
+
+  function handleClick() {
+    setContar(contar + 1)
+    setItems(items => [...items, 'item ' + (contar + 1)])
+  }
+
   return (
-    <>
-      <Modal modal={modal} setModal={setModal} />
-      <ButtonModal modal={modal} setModal={setModal} />
-    </>
+    <h1>
+      <button onClick={handleClick}>{contar}</button>
+      {items.map(item => (
+        <li key={item}>{item}</li>
+      ))}
+    </h1>
   )
 }
 
--- src/ButtonModal.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const ButtonModal = ({ modal, setModal }) => {
-  if (!modal) return <button onClick={() => setModal(true)}>Abrir</button>
-  else return null
-}
-
-export default ButtonModal
