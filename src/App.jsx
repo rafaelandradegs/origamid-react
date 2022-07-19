8f0d5c9d6f7ebccfc4fd991758ad93db9e58b062
Rafael A Goncalves <rafaelandradegs@gmail.com>
2022-07-19 10:58:36 -0300

    Exercicio localStorage minha resolucao

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import Produto from './Produto'
+import React, { useEffect } from 'react'
 
 const App = () => {
-  const [ativo, setAtivo] = React.useState(false)
+  const [listaProduto, setListaProduto] = React.useState(null)
+
+  async function handleClick(e) {
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${e.target.innerText}`
+    )
+    const data = await response.json()
+    setListaProduto(data)
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem('produto') !== null) {
+      setListaProduto(JSON.parse(localStorage.getItem('produto')))
+    }
+  }, [])
+
+  useEffect(
+    () => localStorage.setItem('produto', JSON.stringify(listaProduto)),
+    [listaProduto]
+  )
 
   return (
-    <>
-      {ativo && <Produto />}
-      <button onClick={() => setAtivo(!ativo)}>Ativar</button>
-    </>
+    <div>
+      {listaProduto && (
+        <div>
+          <h1>{listaProduto.nome}</h1>
+          <p>R$ {listaProduto.preco}</p>
+        </div>
+      )}
+      <button onClick={handleClick}>Notebook</button>
+      <button onClick={handleClick}>Smartphone</button>
+    </div>
   )
 }
 
