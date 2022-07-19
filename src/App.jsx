398508f82204c2cea3eadca609874f46067d8523
Rafael A Goncalves <rafaelandradegs@gmail.com>
2022-07-19 09:28:50 -0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
 import React from 'react'
-import Button from './Button'
-import Produtos from './Produtos'
+import Produto from './Produto'
 
 const App = () => {
-  const [dados, setDados] = React.useState(null)
-  const [isLoad, setIsLoad] = React.useState(null)
-
-  async function consumirApi(e) {
-    setIsLoad(true)
-    const response = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${e.target.innerText}`
-    )
-    const json = await response.json()
-    setDados(json)
-    setIsLoad(false)
-  }
+  const [ativo, setAtivo] = React.useState(false)
 
   return (
-    <div>
-      <Button consumirApi={consumirApi}></Button>
-      {isLoad ? <p>Carregando</p> : <Produtos dados={dados} />}
-    </div>
+    <>
+      {ativo && <Produto />}
+      <button onClick={() => setAtivo(!ativo)}>Ativar</button>
+    </>
   )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,4 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
-createRoot(document.querySelector('#root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+createRoot(document.querySelector('#root')).render(<App />)
