c9ff5ffab67dbf60dc37ad4ae1ca5f039d345c1d
Rafael A Goncalves <rafaelandradegs@gmail.com>
2022-08-03 11:04:00 -0300

    textarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,16 @@
 import React from 'react'
 
 const App = () => {
-  const [data, setData] = React.useState([])
-  const [curso, setCurso] = React.useState('')
-
-  React.useEffect(() => {
-    fetch('http://localhost:3010/cursos')
-      .then(response => response.json())
-      .then(json => setData(json))
-  }, [data])
-
-  function novoCurso() {
-    fetch('http://localhost:3010/cursos', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ name: curso })
-    })
-      .then(response => response.json())
-      .then(json => console.log(json))
-    console.log(curso)
-  }
+  const [textarea, setTextarea] = React.useState('')
 
   return (
-    <>
-      <ul>
-        {data.map(d => (
-          <li key={d}>{d}</li>
-        ))}
-      </ul>
-      <input type="text" onChange={({ target }) => setCurso(target.value)} />
-      <button onClick={novoCurso}>Cadastrar Novo Curso</button>
-    </>
+    <form>
+      <textarea
+        value={textarea}
+        onChange={({ target }) => setTextarea(target.value)}
+        rows="5"
+      ></textarea>
+    </form>
   )
 }
 
