e1416baee8da03520717c10cdf16b631905a565b
Rafael A Goncalves <rafaelandradegs@gmail.com>
2022-09-06 08:51:11 -0300

    React Router Dom Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
 import React from 'react'
-import Slide from './Slide'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './Header'
+import Home from './Home'
+import Login from './Login'
+import NaoEncontrado from './NaoEncontrado'
+import Sobre from './Sobre'
 
 const App = () => {
-  const slides = [
-    {
-      id: 'slides1',
-      text: 'Slides 1'
-    },
-    {
-      id: 'slides2',
-      text: 'Slides 2'
-    },
-    {
-      id: 'slides3',
-      text: 'Slides 3'
-    }
-  ]
   return (
-    <div>
-      <Slide slides={slides} />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="sobre" element={<Sobre />} />
+        <Route path="*" element={<NaoEncontrado />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/NaoEncontrado.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NaoEncontrado = () => {
+  return <div>Erro pagina nao encontrada</div>
+}
+
+export default NaoEncontrado
